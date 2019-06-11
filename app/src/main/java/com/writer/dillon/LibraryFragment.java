16233c3ade2b21ac4566cb6110d22ca344d7da1c
Bananajojo123 <aaron.santacruz03@gmail.com>
2019-06-11 12:10:25 -0600

    Added List of all epubs

--- app/src/main/java/com/writer/dillon/LibraryFragment.java
+++ app/src/main/java/com/writer/dillon/LibraryFragment.java
@@ -1,9 +1,16 @@
 package com.writer.dillon;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Environment;
 import android.util.Log;
@@ -15,6 +22,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import android.content.res.AssetManager;
@@ -22,8 +30,12 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.folioreader.FolioReader;
+
 import java.io.InputStream;
 import java.io.FileOutputStream;
+
 
 import nl.siegmann.epublib.domain.Author;
 import nl.siegmann.epublib.domain.Book;
@@ -43,7 +55,9 @@ import java.io.FilenameFilter;
 public class LibraryFragment  extends Fragment  {
     private String TAG = this.getClass().getSimpleName();
     private String root = Environment.getExternalStorageDirectory().toString();
+    private RecyclerView recyclerView;
 
+    Context context;
     public LibraryFragment() {
         // Required empty public constructor
     }
@@ -54,15 +68,38 @@ public class LibraryFragment  extends Fragment  {
                              Bundle savedInstanceState)  {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_library, container, false);
+        final ArrayList <Item> itemList = new ArrayList<Item>();
+
+
+        final LibraryAdapter itemArrayAdapter = new LibraryAdapter(R.layout.library_layout, itemList);
+        recyclerView = view.findViewById(R.id.recyclerViewLibrary);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        context = getContext();
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewLibrary);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(itemArrayAdapter);
+
+
 
         FileFilter fileFilter = new FileFilter();
         File[] files = fileFilter.finder(root);
 
         for(File f : files){
-            Log.i(TAG, getDetails(f.getAbsolutePath()));
+            itemList.add(new Item(f.getName(), f.getAbsolutePath()));
         }
 
 
+
+        ItemClickSupport.addTo(recyclerView)
+                .setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        FolioReader folioReader = FolioReader.get();
+                        folioReader.openBook(itemList.get(position).getPath());
+                    }
+                });
 
         return view;
     }
@@ -81,6 +118,8 @@ public class LibraryFragment  extends Fragment  {
             return dfg.getMessage();
         }
     }
+
+
 
 }
 
